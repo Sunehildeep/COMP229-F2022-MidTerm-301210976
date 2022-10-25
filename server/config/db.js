6a1766447554d47ea61cf167edd2e16839d9fd9b
Sunehildeep Singh <sunehildeepsinghsandhu123@gmail.com>
2022-10-25 11:38:46 -0400

    Added mongodb atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  URI: "mongodb://127.0.0.1/product_store",
+  URI: 'mongodb+srv://sunehildeep:sunehildeep@cluster0.ncv9xdw.mongodb.net/product_store',
 };
